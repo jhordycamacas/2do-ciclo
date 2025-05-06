45a64082d05fa453f119de7631281eb5e896a302
jhordycamacas <jhordycamacas9@gmail.com>
2025-05-06 08:36:03 -0500

    cambs

---
+++ Matricess/src/matricess/Matricess.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package matricess;
+
+/**
+ *
+ * @author utpl
+ */
+public class Matricess {
+
+    /**
+     * @param args the command * @author utpl line arguments
+     */
+    public static void main(String[] args) {
+         Metodos1 metodos = new Metodos1();
+
+        int opc;
+        do {
+            opc = metodos.menu();
+            switch (opc) {
+                case 1 ->
+                    metodos.leerMatriz();
+                case 2 ->
+                    metodos.presentarMatriz();
+                case 3 -> 
+                    metodos.matrizTranspuesta();
+
+            }
+        } while (opc != 0);
+    }
+
+}
+    
+    
+
---
+++ Matricess/src/matricess/Metodos1.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package matricess;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author utpl
+ */
+public class Metodos1 {
+
+    Scanner sc;
+    int[][] matriz = new int[4][4];
+
+    public Metodos1() {
+        this.sc = new Scanner(System.in);
+    }
+
+    public int menu() {
+        System.out.println("\nMENU PRINCIPAL:");
+        System.out.println("[1] Insertar nueva matriz: ");
+        System.out.println("[2] Presentar matriz: ");
+        System.out.println("[3] Matriz Transpuesta: ");
+        System.out.println("[0] Salir: ");
+        return sc.nextInt();
+
+    }
+
+    public void leerMatriz() {
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.println("Ingrese valor para la posicion " + i + " " + j + " :");
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+
+    }
+
+    public void presentarMatriz() {
+        System.out.println("MATRIZ: \n");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.print(matriz[i][j] + "  ");
+            }
+            System.out.println("\n");
+        }
+
+    }
+
+    public void matrizTranspuesta() {
+        int aux = 0;
+        for (int i = 0; i < matriz.length - 1; i++) {
+            for (int j = i+1; j < matriz[0].length; j++) {
+                aux = matriz[i][j];
+                matriz[i][j] = matriz[j][i];
+                matriz[j][i] = aux;
+            }
+
+        }
+
+    }
+
+}
