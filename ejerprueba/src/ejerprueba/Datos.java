fe532899c58556be4682b5758ee7ea0b53b56a96
jhordycamacas <jhordycamacaas9@gmail.com>
2025-05-07 21:30:32 -0500

    cambs

---
+++ ejerprueba/src/ejerprueba/Datos.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejerprueba;
+
+/**
+ *
+ * @author usuario
+ */
+public class Datos {
+    String nombre;
+    String provincia;
+    String ciudad;
+    int cambien;
+    int cammal;
+
+    public Datos(String nombre1, String provincia1, String ciudad1, int cambien1, int cammal1) {
+        this.nombre = nombre1;
+        this.provincia = provincia1;
+        this.ciudad = ciudad1;
+        this.cambien = cambien1;
+        this.cammal = cammal1;
+    }
+    
+    
+           
+}
